df182ad6bbceb4b2e8f35eed692fac91b2cc631c
PietroBucker <108037140+PietroBucker@users.noreply.github.com>
2024-07-15 15:38:15 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/videos/frontend': '/frontend', // Rota específica para videos/frontend
+    '/videos/frontend': 'videos.frontend', // Rota específica para videos/frontend
     '/videos/backend': '/backend' // Rota específica para videos/backend
 }))
 server.use(router)
