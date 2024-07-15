cf07aa6ee417d20a0b38804da257c0b487bbebbd
PietroBucker <108037140+PietroBucker@users.noreply.github.com>
2024-07-15 16:08:08 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/video/*': '/videos/$1'
+    '/videos/*': '/videos/$1'
 }))
 server.use(router)
 server.listen(3000, () => {
